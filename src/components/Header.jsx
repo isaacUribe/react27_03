859dee3788d45fb6f2ac68a397ce7716fd9922ad
401 <401@academicos.cesde.edu.co>
2023-03-27 11:01:50 -0600

    se crea proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Img from "./img"
+
+const Header = () =>{
+    return(
+        <header>
+            <Img/>
+            <nav>
+                
+            </nav>
+        </header>
+    )
+}
+export default Header 
---
+++ src/components/img.jsx
@@ -0,0 +1,8 @@
+import logo from "../../public/vite.svg"
+
+const Img = () =>{
+    return(
+        <img src={logo} alt="Logo"/>
+    )
+}
+export default Img
